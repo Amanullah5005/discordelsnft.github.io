59a87008baea29509be23a9d5c7a73bebcf6b6c6
Amanullah behlim <mamanullahbehlim2005@gmail.com>
2023-03-14 20:07:45 +0500

    Discordels
    A Doodle Style NFT Collection

---
+++ JS/code.js
@@ -0,0 +1,41 @@
+
+
+// Navigation
+function toggleMenu(){
+  const toggleMenu = document.querySelector(".toggleMenu");
+  const navigation = document.querySelector(".navigation");
+toggleMenu.classList.toggle ("active");
+// let element = document.body;
+navigation.classList.toggle ("active");
+}
+// Navigationd End
+
+
+// this is dark light shuffle Function
+function darklight(){
+  const darklight = document.querySelector(".darkLight");
+  darklight.classList.toggle("active");
+  let element = document.body;
+  element.classList.toggle("dark-theme")
+};
+// this is dark light shuffle Function End
+
+
+// Mint Add and Substract Buttons 
+// function add() {
+//     // Get the input element
+//     var input = document.getElementById("numberInput");
+
+//     // Increment the value by 1
+//     input.value = parseInt(input.value) + 1;
+//   }
+
+//   function subtract() {
+//     // Get the input element
+//     var input = document.getElementById("numberInput");
+
+//     // Decrement the value by 1
+//     input.value = parseInt(input.value) - 1;
+//   }
+// Mint Add and Substract Buttons End
+
